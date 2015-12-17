103b6048f149f04a021eca905c3c5fce2d6b1f67
MaAoyu <841613407@qq.com>
2015-12-17 22:38:07 +0800

    delete

--- src/main/java/com/OCare/service/XMLFtpService.java
+++
@@ -1,9 +0,0 @@
-package com.OCare.service;
-
-/**
- * Created by Ma on 2015/11/18.
- */
-public interface XMLFtpService {
-    String downloadFile(String fileName);
-    String uploadDocFile(String fileName);
-}
--- src/main/webapp/pages/templates/footer.jsp
+++
@@ -1,7 +0,0 @@
-
-<div class="footer">
-  <i class="Copyright icon"></i> Copyright Tongji University
-</div>
-
-</body>
-</html>
